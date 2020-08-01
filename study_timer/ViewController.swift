7343a1516dc4b4e7e6df45e303a1b3ff88c0ea22
황선영 <hwang-sunyoung@hwangseon-yeong-ui-MacBookAir.local>
2020-08-01 17:21:20 +0900

    8/1 2주차 프로젝트 진행내용

--- study_timer/ViewController.swift
+++ study_timer/ViewController.swift
@@ -10,20 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var Label_hello: UILabel!
-    @IBOutlet weak var text_num: UITextField!
-    @IBOutlet weak var text_name: UITextField!
+    @IBOutlet weak var label_persent: UILabel!
+    @IBOutlet weak var label_ALLtime: UILabel!
+    @IBOutlet weak var label_sumtime: UILabel!
+    @IBOutlet weak var label_timer: UILabel!
     
-    @IBAction func Button_click(_ sender: UIButton) {
-//        Label_hello.text = "Button Click"
-        Label_hello.text = "학번 : " + text_num.text! + "\n이름 : " + text_name.text!
-    }
+    @IBOutlet weak var button_start_outlet: UIButton!
+    @IBOutlet weak var button_stop_outlet: UIButton!
+    @IBOutlet weak var button_restart_outlet: UIButton!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        button_start_outlet.layer.cornerRadius = 8
+        button_stop_outlet.layer.cornerRadius = 8
+        button_restart_outlet.layer.cornerRadius = 8
     }
 
-
+    @IBAction func button_start_action(_ sender: UIButton) {
+        label_timer.text = "황선영 완료!"
+    }
+    
+    @IBAction func button_stop_action(_ sender: UIButton) {
+        label_timer.text = "종료!"
+    }
+    
+    @IBAction func button_restart_action(_ sender: UIButton) {
+        label_timer.text = "재시작!"
+    }
+    @IBAction func button_reset_action(_ sender: UIButton) {
+        label_timer.text = "초기화!"
+    }
+    
+    @IBAction func button_timeset_action(_ sender: UIButton) {
+        label_timer.text = "시간 설정!"
+    }
 }
 
